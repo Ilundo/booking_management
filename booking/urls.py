f5cc2c4b5f8255d9231595081dc502d887204f56
Ilundo <vanavakaruk4@email.com>
2025-03-08 20:29:26 +0200

    message

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import create_room, rooms_list, tables_list, visitors_list
+from .views import book_room, book_table, create_room, create_table, rooms_list, tables_list, visitors_list
 urlpatterns = [
     path("rooms/list/",rooms_list,name="rooms-list" ),
     path("tables/list/<int:room_id>",tables_list,name="tables-list"),
     path("visitors/list/",visitors_list, name="visitors-list" ),
-    path("room/create/",create_room,name="create-room")
+    path("room/create/",create_room,name="create-room"),
+    path("room/book/", book_room,name="book-room"),
+    path("table/book/", book_table, name="book-table"),
+    path("table/create/",create_table,name="create-table"),
+    
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render , get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
-from .models import  Room, Table, Visitor
+from .models import  BookedRoom, BookedTable, Room, Table, Visitor
 
 
 # Create your views here.
@@ -39,4 +39,47 @@ def create_room(request:HttpRequest):
         tables_num = request.POST.get("tables_num", 0)
 
         Room.objects.create(number=room_number,tables_num=tables_num)
+        return redirect(reverse("rooms-list"))
+
+def create_table(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, "booking/create_table.html")
+
+    if request.method == "POST":
+        table_number = request.POST.get("table_number")
+        room_id = request.POST.get("room_id")
+
+        room = get_object_or_404(Room, id=room_id)
+        Table.objects.create(number=table_number, room=room)
+
+        return redirect(reverse("rooms-list"))
+
+def book_room(request:HttpRequest):
+    if request.method== "GET":
+        return render(request, "booking/book_room.html")
+    if request.method=="POST":
+        room_number = request.POST.get("room_num")
+        visitor_name = request.POST.get("visitor_name")
+        datetime = request.POST.get("datetime")
+
+        room = Room.objects.get(number=room_number)
+        visitor, new= Visitor.objects.get_or_create(name=visitor_name)
+
+        new_booked_room = BookedRoom.objects.create(room = room,visitor=visitor,datetime=datetime)
+
+        return redirect(reverse("rooms-list"))
+    
+def book_table(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, "booking/book_table.html")
+
+    if request.method == "POST":
+        table_number = request.POST.get("table_num")
+        visitor_name = request.POST.get("visitor_name")
+        datetime = request.POST.get("datetime")
+
+        table = Table.objects.get(number=table_number)
+        visitor, new = Visitor.objects.get_or_create(name=visitor_name)
+
+        new_booked_table = BookedTable.objects.create(table = table,visitor=visitor,datetime=datetime)
         return redirect(reverse("rooms-list"))
