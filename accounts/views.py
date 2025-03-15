bfc37d499a8348699003d7968bfb62f736ab0daf
Ilundo <vanavakaruk4@email.com>
2025-03-15 20:23:32 +0200

    Message

---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect, render
+from django.http import HttpRequest 
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+# Create your views here.
+def register(request:HttpRequest):
+    
+    if request.method =="GET":
+        form = UserCreationForm()
+    elif request.method =="POST":
+        form= UserCreationForm(request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("/")
+    return render(request,"registration/register.html", {"form": form})
